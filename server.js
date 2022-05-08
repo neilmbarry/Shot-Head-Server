cdf6a06567446d3ad588e066d577c69dcdaf80a2
neilmbarry <neilmbarry@gmail.com>
2022-05-08 12:14:00 -0400

    setting origin to deployed site

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://shot-head-react.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
